b8e48afaf9306248b6cfedc4f7a1504f908b2a98
Jenna Scholl <bin19005@byui.edu>
2023-03-03 09:41:21 -0700

    created testVelocity and testHowitzer

---
+++ testHowitzer.h
@@ -0,0 +1,21 @@
+/***********************************************************************
+ * Header File:
+ *    Test Howitzer : Test the Howitzer class
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    All the unit tests for Howitzer
+ ************************************************************************/
+
+#pragma once
+
+#include <iostream>
+//#include "howitzer.h"
+#include <cassert>
+
+using namespace std;
+
+class testHowitzer
+{
+
+};
---
+++ testVelocity.h
@@ -0,0 +1,21 @@
+/***********************************************************************
+ * Header File:
+ *    Test Velocity : Test the Velocity class
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    All the unit tests for Velocity
+ ************************************************************************/
+
+#pragma once
+
+#include <iostream>
+#include "velocity.h"
+#include <cassert>
+
+using namespace std;
+
+class testVelocity
+{
+
+};
